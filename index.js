84f18d85a21db61a0d3b471e5b85e2c120a4c63d
Erika Weil <erikaweil@outlook.com>
2017-08-30 17:18:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObj;
 }
 
-function destructielyUpdateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
